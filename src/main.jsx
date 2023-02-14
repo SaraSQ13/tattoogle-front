bc29ffa7575f0907c57b152ec80c6ed9b26e881a
Sara <sara.s.q@hotmail.com>
2023-02-14 13:21:48 +0100

    fix: change files .css to .scss

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import "bootstrap/scss/bootstrap.scss";
 import "bootstrap/dist/js/bootstrap.bundle";
-import './index.css'
+import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
