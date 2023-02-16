1744739b8a3bfcba56bd0aa4f98eae3e553c45ce
Sara <sara.s.q@hotmail.com>
2023-02-16 14:29:59 +0100

    fix: Login form style

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.scss'
-import { BrowserRouter, Routes } from 'react-router-dom'
-import { Header } from './components/Header/Header'
-import { Footer } from './components/Footer/Footer'
-import { Home } from './container/Home/Home'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.scss";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Header } from "./components/Header/Header";
+import { Footer } from "./components/Footer/Footer";
+import { Home } from "./container/Home/Home";
+import Login from "./container/Login/Login";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <BrowserRouter>
-      <Header/>
-      <Home/>
-      <Routes>
-
-      </Routes>
-      <Footer/>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login/>}/>
+        </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/container/Login/Login.jsx
+++ src/container/Login/Login.jsx
@@ -1,51 +1,44 @@
 import "./Login.scss";
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 
 export default function Login() {
+  //hooks
+  const navigate = useNavigate();
+  // navigate("/login");
 
-    //hooks
-    const navigate = useNavigate();
-    navigate("/login");
+  return (
+    <div>
+      <div className="container pt-5 col-lg-3">
+        <h2>Login</h2>
+        <form className="text-start" noValidate>
+          <div className="mb-3">
+            <label className="form-label">Email</label>
+            <input type="email" name="email" className="form-control" />
+            <div id="emailHelp" className="form-text form-text-error"></div>
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Contraseña</label>
+            <input type="password" name="password" className="form-control" />
+            <div id="emailHelp" className="form-text form-text-error"></div>
+          </div>
+          <div className="d-grid gap-2">
+            <button type="submit" className="button">
+              Enviar
+            </button>
+          </div>
+          <hr />
+          <div className="d-grid gap-2">
+          <h5>Si es tu primera vez:</h5>
+          <button type="submit" className="button">
+            <NavLink className="nav-link" to="/register">
+              Regístrate
+            </NavLink>
+            </button>
 
-    return (
-        <div>
-        <div className="container pt-5 col-lg m-auto">
-          <h2>Login</h2>
-          <form className="text-start" noValidate onSubmit={handleSubmit}>
-            <div className="mb-3">
-              <label className="form-label">Email address</label>
-              <input
-                type="email"
-                name="email"
-                className="form-control"
-                value={formValues.email}
-                onChange={handleChange}
-              />
-              <div id="emailHelp" className="form-text form-text-error">
-                {formErrors.email}
-              </div>
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Password</label>
-              <input
-                type="password"
-                name="password"
-                className="form-control"
-                value={formValues.password}
-                onChange={handleChange}
-              />
-              <div id="emailHelp" className="form-text form-text-error">
-                {formErrors.password}
-              </div>
-            </div>
-            <div className="d-grid gap-2">
-              <button type="submit" className="button">
-                Submit
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>  
-    );
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
