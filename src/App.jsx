9c6a0c86bf9dba07b1741aa6de3dc7f332b23437
Sara <sara.s.q@hotmail.com>
2023-02-16 20:39:09 +0100

    feat: created structure Tattoer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.scss";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Header } from "./components/Header/Header";
@@ -7,18 +6,18 @@ import { Footer } from "./components/Footer/Footer";
 import { Home } from "./container/Home/Home";
 import Login from "./container/Login/Login";
 import Register from "./container/Register/Register";
+import TattoersList from "./container/TattoersList/TattoersList";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <BrowserRouter>
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/register" element={<Register/>}/>
+          <Route path="/tattoers" element={<TattoersList />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
         <Footer />
       </BrowserRouter>
--- src/container/TattoersList/TattoersList.jsx
+++ src/container/TattoersList/TattoersList.jsx
@@ -1,1 +1,12 @@
 import "./TattoersList.scss";
+import React from "react";
+import Tattoer from "../../components/Tattoer/Tattoer";
+
+export default function TattoersList() {
+    return (
+        <div>
+        <h1>Aquí va la lista de tatuadores</h1>
+    
+        </div>
+    )
+}
