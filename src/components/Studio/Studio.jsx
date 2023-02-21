c68869f263e177efac4ededd948e2187df833b6c
Sara <sara.s.q@hotmail.com>
2023-02-21 23:59:23 +0100

    feat: created Studio component

---
+++ src/components/Studio/Studio.jsx
@@ -0,0 +1,33 @@
+import "./Studio.scss";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function Studio({ studio }) {
+  const navigate = useNavigate();
+
+  const getSingleStudio = (name) => {
+    navigate(`/studios/${name}`);
+  };
+
+  return (
+    <div className="card" >
+      <img src="..." className="card-img-top" alt="Foto del artista" />
+      <div className="card-body">
+        <h5 className="card-title">{studio.studio_name}</h5>
+      </div>
+      <ul className="list-group list-group-flush">
+        <li className="list-group-item">{studio.city}</li>
+        <li className="list-group-item">{studio.style}</li>
+        <li className="list-group-item">A third item</li>
+      </ul>
+      <div className="card-body">
+        <a href="#" className="card-link">
+          Card link
+        </a>
+        <a href="#" className="card-link">
+          Another link
+        </a>
+      </div>
+    </div>
+  );
+}
