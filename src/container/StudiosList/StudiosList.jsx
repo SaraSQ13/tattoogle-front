0cbc1db5534a960d2bee03c3d87133859e0192eb
Sara <sara.s.q@hotmail.com>
2023-02-22 00:12:32 +0100

    feat: adding hooks and data at StudiosList component

--- src/container/StudiosList/StudiosList.jsx
+++ src/container/StudiosList/StudiosList.jsx
@@ -1,8 +1,32 @@
 import "./StudiosList.scss";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Studio from "../../components/Studio/Studio";
+import { StudioService } from "../../_services/StudioService";
 
 export default function StudiosList() {
-    return (
-        <h1>Aquí van los estudios</h1>
-    )
+  //hooks
+  const [studios, setStudios] = useState([]);
+
+  useEffect(() => {
+    getAllStudios();
+  }, []);
+
+  //function
+  const getAllStudios = async () => {
+    try {
+      const res = await StudioService.getAllStudios();
+      setStudios(res);
+    } catch (error) {
+      console.log(error.message || error);
+    }
+  };
+  return (
+    <div className= "container">
+      <h1>ESTUDIOS DE TATUAJE</h1>
+      <div className="tattoer-list">
+        {studios.length > 0 &&
+          studios.map((stu) => <Studio key={stu._id} studio={stu} />)}
+      </div>
+    </div>
+  );
 }
