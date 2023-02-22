a19f718f8ae25d0855dc6f2501eb23cae2c254c5
Sara <sara.s.q@hotmail.com>
2023-02-22 18:00:59 +0100

    feat: added hooks & handlers

--- src/container/Login/Login.jsx
+++ src/container/Login/Login.jsx
@@ -1,26 +1,83 @@
 import "./Login.scss";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
+import AuthService from "../../_services/AuthService";
+import TokenStorageService from "../../_services/TokenStorageService";
+import { validateLoginFormValues } from "../../_helpers/form-utilities";
 
 export default function Login() {
   //hooks
   const navigate = useNavigate();
-  
+
+  const initialValues = {
+    email: "",
+    password: "",
+  };
+
+  const [formValues, setFormValues] = useState({ email: "", password: "" });
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  useEffect(() => {
+    const credentials = {
+      email: formValues.email,
+      password: formValues.password,
+    };
+
+    //verificar que no hay error
+    if (Object.keys(formErrors).length == 0 && isSubmit) {
+      console.log("LOGIN...");
+      handleLogin(credentials);
+    }
+  }, [isSubmit, formErrors]);
+
+  const handleLogin = async (credentials) => {
+    try {
+      const res = await AuthService.login(credentials);
+      console.log(res.data);
+      TokenStorageService.saveToken(res.data.token);
+      sessionStorage.setItem("userId", res.data.id);
+      console.log(res.data.user);
+
+      if (res.data.user.role == "super_admin") {
+        navigate("/admin");
+      } else if (res.data.user.role == "user") {
+        navigate("/tattoer");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  //handlers 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validateLoginFormValues(formValues));
+    setIsSubmit(true);
+  };
 
   return (
     <div>
       <div className="container pt-5 col-lg-3">
         <h2>Login</h2>
-        <form className="text-start" noValidate>
+        <form className="text-start" noValidate onSubmit={handleSubmit}>
           <div className="mb-3">
             <label className="form-label">Email</label>
-            <input type="email" name="email" className="form-control" />
+            <input type="email" name="email" className="form-control" value={formValues.email} onChange={handleChange}/>
             <div id="emailHelp" className="form-text form-text-error"></div>
           </div>
           <div className="mb-3">
             <label className="form-label">Contraseña</label>
-            <input type="password" name="password" className="form-control" />
-            <div id="emailHelp" className="form-text form-text-error"></div>
+            <input type="password" name="password" className="form-control" value={formValues.password} onChange={handleChange} />
+            <div id="emailHelp" className="form-text form-text-error">{formErrors.password}</div>
           </div>
           <div className="d-grid gap-2">
             <button type="submit" className="button">
@@ -29,13 +86,12 @@ export default function Login() {
           </div>
           <hr />
           <div className="d-grid gap-2">
-          <h5>Si es tu primera vez:</h5>
-          <button type="submit" className="button">
-            <NavLink className="nav-link" to="/register">
-              Regístrate
-            </NavLink>
+            <h5>Si es tu primera vez:</h5>
+            <button type="submit" className="button">
+              <NavLink className="nav-link" to="/register">
+                Regístrate
+              </NavLink>
             </button>
-
           </div>
         </form>
       </div>
