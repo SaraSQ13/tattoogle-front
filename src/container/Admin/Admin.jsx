2141a7d3fc665385007a1480330a5a5e743057d9
Sara <sara.s.q@hotmail.com>
2023-02-23 01:01:21 +0100

    feat: created admin panel, added hooks

--- src/container/Admin/Admin.jsx
+++ src/container/Admin/Admin.jsx
@@ -1,10 +1,41 @@
 import "./Admin.scss";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import TokenStorageService from "../../_services/TokenStorageService";
+import UserService from "../../_services/UserService";
 
 export default function Admin() {
-    return (
-        <div>
-            <h1>Aquí va el panel de Admin</h1>
+  //hooks
+  const [users, setUsers] = useState([]);
+  const token = TokenStorageService.getToken();
+
+  useEffect(() => {
+    getAllUsers(token);
+  }, []);
+
+  const getAllUsers = async (token) => {
+    try {
+      const res = await UserService.getAllUsers(token);
+      setUsers(res.data.data);
+      console.log(res.data.data);
+    } catch (error) {
+      console.log(error.message || error);
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1>Admin</h1>
+      {users.map((user)=> (
+      <div className="card" key={user._id}>
+        <div className="card-body">
+          <h5 className="card-title">Usuario: {user.name}</h5>
         </div>
-    )
+        <ul className="list-group list-group-flush">
+          <li className="  list-group-item">Email: {user.email}</li>
+          <li className="  list-group-item">Rol: {user.role}</li>
+        </ul>
+      </div>
+      ))}
+    </div>
+  );
 }
